84ffa127de3cc705b103d3c89ed2ab49eccfbd0d
Dan Barrett <dan@danbarrett.com.au>
2022-10-11 17:48:40 +1100

    More Symfony 6 fixes

--- JMoseCommandSchedulerBundle.php
+++ JMoseCommandSchedulerBundle.php
@@ -2,8 +2,15 @@
 
 namespace JMose\CommandSchedulerBundle;
 
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class JMoseCommandSchedulerBundle extends Bundle
 {
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        $class = $this->getContainerExtensionClass();
+
+        return new $class();
+    }
 }
